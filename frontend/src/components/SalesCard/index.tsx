76e083f9c015d190cf5856e7a04ad2948a714a9e
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-11-19 19:07:36 -0300

    Date Update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -8,16 +8,23 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./styles.css";
 
 const SalesCard = () => {
-  useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+
 
   const [minDate, setMinDate] = useState(new Date());
   const [maxDate, setMaxDate] = useState(new Date());
 
   const [sales, setSales] = useState<Sale[]>([]);
+
+
+  useEffect(() => {
+
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
+      setSales(response.data.content);
+    });
+  }, [minDate, maxDate]);
 
   return (
     <>
