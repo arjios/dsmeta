2a95952ec1f3f888f0c890de212c83a0581500c2
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-11-20 17:39:41 -0300

    DSMeta

--- backend/src/main/java/com/ios/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/ios/dsmeta/repositories/SaleRepository.java
@@ -13,7 +13,7 @@ import com.ios.dsmeta.entities.Sale;
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 	
-	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")	
 	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
